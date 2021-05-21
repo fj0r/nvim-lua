2b61da7fe00e8f53c836f3e56a677d3cb1e74733
nash <nash@iffy.me>
2021-05-22 00:02:04 +0800

    .

--- lua/addons/tree.lua
+++ lua/addons/tree.lua
@@ -11,6 +11,7 @@ g.nvim_tree_follow = 1 --0 by default, this option allows the cursor to be updat
 g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
 g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and folders starting with a dot `.`
 g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
+g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_root_folder_modifier = ':~' --This is the default. See :help filename-modifiers for more options
 g.nvim_tree_tab_open = 0 --0 by default, will open the tree when entering a new tab and the tree was previously open
 g.nvim_tree_width_allow_resize  = 1 --0 by default, will not resize the tree when opening a file
--- lua/lang/lsp/yamlls.lua
+++ lua/lang/lsp/yamlls.lua
@@ -2,7 +2,15 @@ require'lspconfig'.jsonls.setup {
 }
 
 require'lspconfig'.yamlls.setup {
-    schemas = {
-        [vim.g.config_root .. '/JSONSchema/compose-spec.json'] = "/docker-compose.yml",
+    settings = {
+        ['yaml.schemas'] = {
+            [vim.g.config_root .. '/JSONSchema/compose-spec.json'] = {"/docker-compose.yml"} ,
+            kubernetes =  { "/*" }
+        },
+        ['yaml.completion'] = true,
+        ['yaml.format.singleQuote'] = true,
+        ['yaml.hover'] = true,
+        ['yaml.validate'] = true,
+        ['yaml.trace.server'] = 'verbose', -- enum { "off", "messages", "verbose" }
     }
 }
