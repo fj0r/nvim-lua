85a89052fe31fdf9adb6972e822890e70b79b44d
nash <nash@iffy.me>
2021-05-24 23:57:19 +0800

    compe clear

--- lua/addons/compe.lua
+++ lua/addons/compe.lua
@@ -1,33 +1,33 @@
 vim.o.completeopt = "menuone,noselect"
 
 require'compe'.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
+    enabled          = true;
+    autocomplete     = true;
+    debug            = false;
+    min_length       = 1;
+    preselect        = 'enable';
+    throttle_time    = 80;
+    source_timeout   = 200;
     incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
+    max_abbr_width   = 100;
+    max_kind_width   = 100;
+    max_menu_width   = 100;
+    documentation    = true;
 
     source = {
-        path = {kind = " (Path)"},
-        buffer = {kind = " (Buffer)"},
-        calc = {kind = " (Calc)"},
-        vsnip = {kind = " (Snippet)"},
-        nvim_lsp = {kind = " (LSP)"},
-        nvim_lua = false,
-        spell = {kind = " (Spell)"},
-        tags = false,
+        path                  = true,
+        buffer                = true,
+        calc                  = true,
+        vsnip                 = true,
+        nvim_lsp              = true,
+        nvim_lua              = false,
+        spell                 = true,
+        tags                  = false,
         vim_dadbod_completion = true,
         -- snippets_nvim = {kind = "  "},
         -- ultisnips = {kind = "  "},
         -- treesitter = {kind = "  "},
-        emoji = {kind = " (Emoji)", filetypes={"markdown", "text"}}
+        emoji = {filetypes={"markdown", "text"}}
     };
 }
 
