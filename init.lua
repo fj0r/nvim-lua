094ba233266128def840626d50f05c837693c392
nash <nash@iffy.me>
2021-05-26 13:28:49 +0800

    disable: nvim-dap-virtual-text

--- init.lua
+++ init.lua
@@ -203,7 +203,7 @@ packer.startup(function(use)
         config = [[require 'lang.dap']],
     }
     use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
-    use {'theHamsta/nvim-dap-virtual-text', requires = {'mfussenegger/nvim-dap'} }
+    use {'theHamsta/nvim-dap-virtual-text', disable = true, requires = {'mfussenegger/nvim-dap'} }
 
     use 'tami5/sql.nvim'
     use 'nanotee/sqls.nvim'
