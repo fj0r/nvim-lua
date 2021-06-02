ab8c0247305cf42c987db97098a4bc8508943cfb
nash <nash@iffy.me>
2021-06-02 09:26:50 +0800

    nu as default shell

--- lua/addons/toggleterm.lua
+++ lua/addons/toggleterm.lua
@@ -13,7 +13,7 @@ require'toggleterm'.setup{
     -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
     direction = 'horizontal',
     close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell, -- change the default shell
+    shell = require'utils'.which'nu', -- change the default shell
     -- This field is only relevant if direction is set to 'float'
     float_opts = {
         -- The border key is *almost* the same as 'nvim_win_open'
