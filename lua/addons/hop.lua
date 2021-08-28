2f4042a7dadc256b1e393b9d14ff67ebeda339e7
nash <nash@iffy.me>
2021-08-28 11:15:09 +0800

    hop

--- lua/addons/hop.lua
+++ lua/addons/hop.lua
@@ -2,9 +2,9 @@ local m = vim.api.nvim_set_keymap
 for _, o in ipairs({'n', 'v', 'x'}) do
     m(o, '<leader><space>', "<cmd>lua require'hop'.hint_words()<cr>", {})
     m(o, 's', "<cmd>lua require'hop'.hint_words()<cr>", {})
-    m(o, '<leader>j', "<cmd>lua require'hop'.hint_lines()<cr>", {})
-    m(o, '<C-s>', "<cmd>lua require'hop'.hint_lines()<cr>", {})
-    m(o, '<leader>k', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", {})
+    m(o, '<leader>j', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", {})
+    m(o, '<leader>k', "<cmd>lua require'hop'.hint_lines()<cr>", {})
+    m(o, '<C-s>', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", {})
     m(o, '<leader>c', "<cmd>lua require'hop'.hint_char1()<cr>", {})
     m(o, '<leader>z', "<cmd>lua require'hop'.hint_char2()<cr>", {})
     m(o, '<leader>p', "<cmd>lua require'hop'.hint_patterns()<cr>", {})
