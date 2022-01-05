6ff2cbb78119cca27ef3bf8569de9d841ee314c4
nash <nash@iffy.me>
2022-01-05 14:23:27 +0800

    rq

--- lua/lang/dap/common.lua
+++ lua/lang/dap/common.lua
@@ -54,7 +54,7 @@ dap.configurations.rust = {
             r:close()
             --local cwd = lspconfig.util.find_git_ancestor(vim.fn.getcwd())
             local cwd = lspconfig.util.root_pattern('Cargo.toml')(vim.fn.getcwd())
-            local handle = io.popen("tomlq package.name -f " .. cwd .. '/Cargo.toml')
+            local handle = io.popen("cat "..cwd.."/Cargo.toml| rq -t | jq -r '.package.name'")
             local name = handle:read("*l")
             handle:close()
             return cwd .. "/target/debug/" .. name
