4ffe9e57be0fd2b49ab13dbadbc3231307a9e514
nash <nash@iffy.me>
2022-04-10 01:17:36 +0800

    asynctask.ini

--- lua/addons/asyncrun.lua
+++ lua/addons/asyncrun.lua
@@ -1,1 +1,4 @@
+vim.g.asynctasks_extra_config = {
+    vim.g.config_root .. '/asynctask.ini'
+}
 vim.api.nvim_set_keymap('n', '<leader>t', "<cmd>lua require('telescope').extensions.asynctasks.all()<cr>", { noremap = true })
