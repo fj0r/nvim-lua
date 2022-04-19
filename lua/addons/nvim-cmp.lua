49ef1dab3ee9a1fffcfeed7678ef84e3800dd4e3
nash <nash@iffy.me>
2022-04-19 09:53:04 +0800

    nvim-cmp keymap

--- lua/addons/nvim-cmp.lua
+++ lua/addons/nvim-cmp.lua
@@ -8,13 +8,13 @@ cmp.setup {
             require('luasnip').lsp_expand(args.body)
         end,
     },
-    mapping = {
+    mapping = cmp.mapping.preset.insert {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
@@ -38,7 +38,7 @@ cmp.setup {
             end
         end,
     },
-    sources = {
+    sources = cmp.config.sources {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
     }, {
