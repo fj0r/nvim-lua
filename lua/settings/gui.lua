e1f29ed3d5811f3dcb3deed5ebf78507fa6cb460
nash <nash@iffy.me>
2022-04-26 14:41:01 +0800

    neovide

---
+++ lua/settings/gui.lua
@@ -0,0 +1,5 @@
+vim.g.neovide_font_subpixel_antialiasing = 1
+vim.o.guifont = "JetBrains Mono Light:h11"
+vim.g.neovide_fullscreen = true
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_cursor_vfx_mode = "railgun"
--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -3,4 +3,5 @@ require 'settings.keymap'
 require 'settings.data'
 require 'settings.filetype'
 require 'settings.colors'
+require 'settings.gui'
 
