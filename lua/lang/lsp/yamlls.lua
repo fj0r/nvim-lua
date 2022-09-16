db9865a7f07d546f27fe1a4e1fb4998e9cbfa334
nash <nash@iffy.me>
2022-09-17 00:51:09 +0800

    vim.g: json_schemas & yaml_schemas

--- lua/lang/lsp/yamlls.lua
+++ lua/lang/lsp/yamlls.lua
@@ -1,19 +1,23 @@
+vim.g.json_schemas = require('schemastore').json.schemas {
+    select = {
+        'VSCode Code Snippets',
+        'cspell',
+        'Deno',
+        'dockerd.json',
+    },
+    replace = {
+        ['package.json'] = {
+            description = 'package.json overriden',
+            fileMatch = { 'package.json' },
+            name = 'package.json',
+            url = 'https://example.com/package.json',
+        },
+    }
+}
 require'lspconfig'.jsonls.setup {
     settings = {
         json = {
-            schemas = require('schemastore').json.schemas {
-                ignore = {
-                    '.eslintrc',
-                },
-                replace = {
-                    ['package.json'] = {
-                        description = 'package.json overriden',
-                        fileMatch = { 'package.json' },
-                        name = 'package.json',
-                        url = 'https://example.com/package.json',
-                    },
-                }
-            },
+            schemas = vim.g.json_schemas,
             validate = { enable = true },
             colorDecorators = { enable = true },
             format = { enable = true },
@@ -23,7 +27,18 @@ require'lspconfig'.jsonls.setup {
     }
 }
 
-local config = require'syncJSONSchema':config()
+local yaml_json_schemas = require('schemastore').json.schemas {
+    select = {
+        'Helm Chart.yaml',
+        'GitHub Action',
+        'GitHub Workflow',
+        'gitlab-ci',
+    }
+}
+local remote_schemas = {}
+vim.tbl_map(function(schema) remote_schemas[schema.url] = schema.fileMatch end, yaml_json_schemas)
+local local_schemas = require'syncJSONSchema':config()
+vim.g.yaml_schemas = vim.tbl_extend('force', remote_schemas, local_schemas)
 require'lspconfig'.yamlls.setup {
     settings = {
         yaml = {
@@ -33,18 +48,7 @@ require'lspconfig'.yamlls.setup {
             hover = true,
             validate = true,
             trace = { server = 'verbose' },
-            schemas = config
-            --[[
-            schemas = require('schemastore').json.schemas {
-                select = {
-                    'docker-compose',
-                    'gitlab-ci',
-                    'kustomization',
-                    'openapi',
-                    'traefik',
-                }
-            }
-            --]]
+            schemas = vim.g.yaml_schemas
         }
     }
 }
