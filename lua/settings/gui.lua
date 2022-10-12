bb78ba2cc4ac595fe2cd75fbae15a4548887a483
nash <nash@iffy.me>
2022-10-12 23:50:28 +0800

    gui(neovide) settings

--- lua/settings/gui.lua
+++ lua/settings/gui.lua
@@ -1,5 +1,17 @@
-vim.g.neovide_font_subpixel_antialiasing = 0
 vim.o.guifont = "JetBrains Mono Light:h9"
+-- neovide --multigrid
+vim.g.neovide_scale_factor = 0.9
+vim.g.neovide_font_subpixel_antialiasing = 1
 vim.g.neovide_fullscreen = true
 vim.g.neovide_remember_window_size = true
-vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_transparency = 1
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_underline_automatic_scaling = true
+
+vim.g.neovide_cursor_vfx_mode = "railgun" --"sonicboom" --"wireframe"
+vim.g.neovide_cursor_vfx_particle_lifetime = 2
+vim.g.neovide_cursor_vfx_particle_density = 12.0
+vim.g.neovide_cursor_vfx_particle_speed = 10.0
+vim.g.neovide_cursor_vfx_particle_phase = 10.0
