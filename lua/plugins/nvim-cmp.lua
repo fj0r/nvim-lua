2335092b07dbbb606fefd66d2823163087acb32d
nash <nash@iffy.me>
2023-02-12 13:15:45 +0800

    fix cmp sources

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -66,7 +66,6 @@ cmp.setup {
     sources = cmp.config.sources {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-    }, {
         { name = 'buffer' },
         { name = 'path' },
     }
