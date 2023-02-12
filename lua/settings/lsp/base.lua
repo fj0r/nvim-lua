19c15deacdaf94eabcfe090a816e98d5523407fa
nash <nash@iffy.me>
2023-02-12 13:34:09 +0800

    upgrade server_capabilities

--- lua/settings/lsp/base.lua
+++ lua/settings/lsp/base.lua
@@ -23,25 +23,37 @@ local on_attach = function (client, bufnr)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.server_capabilities.document_formatting then
+    if client.server_capabilities.documentFormattingProvider then
         vim.keymap.set("n", "[f", vim.lsp.buf.formatting, bufopts)
     end
-    if client.server_capabilities.document_range_formatting then
+    if client.server_capabilities.documentRangeFormattingProvider then
         vim.keymap.set("v", "[f", vim.lsp.buf.range_formatting, bufopts)
     end
 
     -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.document_highlight then
-        vim.api.nvim_exec([[
-        hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-        hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-        hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-        augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-        ]], false)
+    if client.server_capabilities.documentHighlightProvider then
+        vim.cmd [[
+        hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+        hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+        hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+        ]]
+        vim.api.nvim_create_augroup('lsp_document_highlight', {
+            clear = false
+        })
+        vim.api.nvim_clear_autocmds({
+            buffer = bufnr,
+            group = 'lsp_document_highlight',
+        })
+        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            group = 'lsp_document_highlight',
+            buffer = bufnr,
+            callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            group = 'lsp_document_highlight',
+            buffer = bufnr,
+            callback = vim.lsp.buf.clear_references,
+        })
     end
 end
 
