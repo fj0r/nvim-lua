3409f00e2eba812882e152ba0f4b2a018bc39669
nash <nash@iffy.me>
2023-12-11 09:19:11 +0800

    revert `disable cmdline /`

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -91,14 +91,12 @@ cmp.setup.filetype('NeogitCommitMessage', {
 
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
---[[
 cmp.setup.cmdline('/', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = 'buffer' }
     }
 })
---]]
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
