9e99eb233f22b7fe5490c20bc55ac48f1a290f3d
nash <nash@iffy.me>
2023-12-24 12:17:03 +0800

    swap ' `

--- lua/settings/common/keymap.lua
+++ lua/settings/common/keymap.lua
@@ -11,6 +11,9 @@ require('setup').keymap_table {
     { vim.g.arrow_keys.k,      "v:count == 0 ? 'gk' : 'k'",                   'nse', mode = 'nv',   disabled = not jk_wrap },
     { vim.g.arrow_keys.j,      "v:count == 0 ? 'gj' : 'j'",                   'nse', mode = 'nv',   disabled = not jk_wrap },
     { '<M-o>',                 '<C-f>',                                       'n',   mode = 'c' },
+    -- goto marker
+    { "`",                     "'",                                           'ns' },
+    { "'",                     "`",                                           'ns' },
     -- Go to home and end using capitalized directions
     { 'H',                     '^',                                           'ns' },
     { 'L',                     '$',                                           'ns' },
