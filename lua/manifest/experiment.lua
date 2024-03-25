7315bab402fd29f146408a464f996706ae910a1f
nash <nash@iffy.me>
2024-03-25 16:04:45 +0800

    experiment.lua

---
+++ lua/manifest/experiment.lua
@@ -0,0 +1,28 @@
+local h = require('lazy_helper')
+
+return {
+    {
+        "LintaoAmons/scratch.nvim",
+        event = "VeryLazy",
+        enabled = vim.g.nvim_level >= 2,
+    },
+    {
+        "michaelb/sniprun",
+        config = h.plugins 'sniprun',
+        enabled = vim.g.nvim_level >= 3,
+    },
+    {
+        'NTBBloodbath/rest.nvim',
+        cmd = {
+            'RestNvim', 'RestNvimPreview'
+        },
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {},
+        enabled = vim.g.nvim_level >= 2,
+    },
+    {
+        'rafcamlet/nvim-luapad',
+        cmd = { 'Luapad', 'LuaRun' },
+        enabled = vim.g.nvim_level >= 3,
+    },
+}
