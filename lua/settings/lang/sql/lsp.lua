62fc0699b582db490c97986c6aa5d466b6523bbb
nash <nash@iffy.me>
2024-05-21 15:45:26 +0800

    postgres_lsp

--- lua/settings/lang/sql/lsp.lua
+++ lua/settings/lang/sql/lsp.lua
@@ -1,1 +1,11 @@
-require 'lspconfig'.sqlls.setup {}
+-- require 'lspconfig'.sqlls.setup {}
+
+require('lspconfig.configs').postgres_lsp = {
+  default_config = {
+    name = 'postgres_lsp',
+    cmd = {'postgres_lsp'},
+    filetypes = {'sql'},
+    single_file_support = true,
+    root_dir = require('lspconfig.util').root_pattern 'root-file.txt'
+  }
+}
