f29996de6f50f4a87b30073377413aadcffc773c
nash <nash@iffy.me>
2024-05-27 14:38:53 +0800

    luasnip on_the_fly

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -23,6 +23,26 @@ local m = extras.m
 local l = extras.l
 local postfix = require "luasnip.extras.postfix".postfix
 
+-- on the fly
+local abs_otf_snip = function()
+    local a = vim.fn.getpos('v')
+    local b = vim.fn.getpos('.')
+    local st = vim.fn.getregion(a, b, { type = vim.fn.visualmode() })
+    vim.fn.setreg(vim.v.register, st)
+    vim.api.nvim_buf_set_text(0, a[2]-1, a[3]-1, b[2]-1, b[3], {})
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes("<Esc>",
+            true, false, true), "n", true)
+    require('luasnip.extras.otf').on_the_fly(vim.v.register)
+end
+
+local app_otf_snip = function()
+    require('luasnip.extras.otf').on_the_fly(vim.v.register)
+end
+
+vim.keymap.set('v', '<M-s>', abs_otf_snip, { noremap = true, silent = true })
+vim.keymap.set({'n', 'i'}, '<M-s>', app_otf_snip, { noremap = true, silent = true })
+
 ls.add_snippets("all", {
     s("ternary", {
         -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
