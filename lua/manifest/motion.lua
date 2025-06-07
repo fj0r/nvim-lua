f7549717a4ef7abfe52f0b2d992202889a6c50d8
nash <fj0rd@qq.com>
2025-06-07 09:36:09 +0800

    hop + flash

--- lua/manifest/motion.lua
+++ lua/manifest/motion.lua
@@ -10,14 +10,24 @@ return {
         ---@type Flash.Config
         opts = {},
         keys = {
-            { "s",                mode = { "n", "x", "o" }, 'search' },
-            { "S",                mode = { "n", "o" },      'treesitter' },
-            { "r",                mode = { "o" },           'remote' },
-            { "R",                mode = { "o", "x" },      'treesitter_search' },
-            { "<c-s>",            mode = { "c" },           'toggle' },
-            { '<leader>;',        mode = { 'n', 'x' },      'jump_to_line' },
-            { '<leader><leader>', mode = { 'n', 'x' },      'any_word' },
+            { "s",     mode = { "n", "x", "o" }, 'search' },
+            { "S",     mode = { "n", "o" },      'treesitter' },
+            { "r",     mode = { "o" },           'remote' },
+            { "R",     mode = { "o", "x" },      'treesitter_search' },
+            { "<c-s>", mode = { "c" },           'toggle' },
+            -- { '<leader>;',        mode = { 'n', 'x' },      'jump_to_line' },
+            -- { '<leader><leader>', mode = { 'n', 'x' },      'any_word' },
         },
+    },
+    {
+        'smoka7/hop.nvim',
+        keys = {
+            -- { 'f', 'char1',     desc = 'hop char1',      mode = { 'n', 'x' } },
+            { '<leader><leader>', 'somewhere', desc = 'hop hint_words', mode = { 'n', 'x', 'o' } },
+            { '<leader>;',        'lines',     desc = 'hop hint_lines', mode = { 'n', 'x', 'o' } },
+        },
+        enabled = true,
+        config = h.settings 'hop',
     },
     {
         'chaoren/vim-wordmotion',
@@ -27,31 +37,4 @@ return {
         end
     },
     'wellle/targets.vim',
-    {
-        'ggandor/lightspeed.nvim',
-        keys = {
-            { 's', '<Plug>Lightspeed_omni_s',  desc = 'Lightspeed_omni_s' },
-            { 'S', '<Plug>Lightspeed_omni_gs', desc = 'Lightspeed_omni_gs' },
-        },
-        config = h.settings 'lightspeed',
-        enabled = false,
-    },
-    {
-        'ggandor/leap.nvim',
-        keys = {
-            { m('s', true), 'bi', mode = { 'n', 'x', 'o' } },
-        },
-        config = h.settings 'leap',
-        enabled = false,
-    },
-    {
-        'smoka7/hop.nvim',
-        keys = {
-            --{ 'f', 'char1',     desc = 'hop char1',      mode = { 'n', 'x' } },
-            { '<leader><leader>', 'somewhere', desc = 'hop hint_words', mode = { 'n', 'x', 'o' } },
-            { 'F',                'lines',     desc = 'hop hint_lines', mode = { 'n', 'x', 'o' } },
-        },
-        enabled = false,
-        config = h.settings 'hop',
-    },
 }
