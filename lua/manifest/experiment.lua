cf01c10022e5c3b7b488338af76c065503e635e1
fj0r <fj0rd@qq.com>
2026-02-01 17:18:42 +0800

    nerdy

--- lua/manifest/experiment.lua
+++ lua/manifest/experiment.lua
@@ -3,6 +3,20 @@ local h = require('lazy_helper')
 return {
     -- TODO: remove
     {
+        '2kabhishek/nerdy.nvim',
+        dependencies = {
+            'folke/snacks.nvim',
+        },
+        cmd = 'Nerdy',
+        opts = {
+            max_recents = 30, -- Configure recent icons limit
+            copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+            copy_register = '+', -- Register to use for copying (if `copy_to_clipboard` is true)
+        },
+        config = h.settings 'nerdy',
+        enabled = vim.g.nvim_level >= 2,
+    },
+    {
         "LintaoAmons/scratch.nvim",
         tag = "v0.13.2",
         event = "VeryLazy",
